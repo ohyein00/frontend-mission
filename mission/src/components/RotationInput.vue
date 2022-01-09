7316d551b417df6bf5319b5aea489055e4d4957e
yein <gpg7913@gmail.com>
2022-01-09 15:54:34 +0900

    Feat : 인풋 컴포넌트 생성, 입출력과 로테이션 테스트
    #1 인풋값을 아래 태그에 출력
    #2 버튼 클릭시 인풋 데이터를 로테이션

---
+++ mission/src/components/RotationInput.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="mission_wrap">
+    <div class="input_area">
+      <form>
+        <label>
+          <!-- v-bind로 할 경우 한글 마지막 글자가 출력이 안됨 -->
+          <input
+            :value="txtData"
+            @input="txtData = $event.target.value"
+            type="text"
+            placeholder="텍스트를 입력해주세요"
+          />
+        </label>
+        <button @click="mixText(txtData)" type="button">MIX</button>
+      </form>
+    </div>
+    <p id="resultTxt">{{ txtData }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Mission',
+  props: {
+    initTxtData: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      txtData: this.initTxtData,
+    };
+  },
+  methods: {
+    mixText(data) {
+      this.txtData = data.slice(1) + data.slice(0, 1);
+    },
+  },
+};
+</script>
--- mission/src/views/Home.vue
+++ mission/src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Mission></Mission>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import Mission from '@/components/RotationInput.vue';
 
 export default {
   name: 'Home',
   components: {
+    Mission,
     HelloWorld,
   },
 };
