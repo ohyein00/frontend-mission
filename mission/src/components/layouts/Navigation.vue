6e7a63c23aec34260fdb1c615f60cbe65328218d
yein <gpg7913@gmail.com>
2022-02-06 21:33:17 +0900

    Fix : Navigation 데이터 프로퍼티로 수정 및 라우팅 구현

--- mission/src/components/layouts/Navigation.vue
+++ mission/src/components/layouts/Navigation.vue
@@ -2,29 +2,16 @@
   <div class="nav-wrapper">
     <nav class="nav-area">
       <ul class="nav-list">
-        <li class="nav-item nav-home">
+        <li v-for="nav in navItems" :key="nav.name"
+            @click="pushRouter(nav.path)"
+            :class="{'is-active':name === nav.name}"
+            class="nav-item">
           <div class="icon-area">
-            <font-awesome-icon icon="home"></font-awesome-icon></div>
-          <p>홈</p>
-        </li>
-        <li class="nav-item nav-bars">
-          <div class="icon-area">
-            <font-awesome-icon icon="heart"></font-awesome-icon>
+            <font-awesome-icon :icon="nav.icon"></font-awesome-icon>
           </div>
-          <p>찜 목록</p>
+          <p>{{ nav.title }}</p>
         </li>
-        <li class="nav-item nav-search">
-          <div class="icon-area">
-            <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
-          </div>
-          <p>장바구니</p>
-        </li>
-        <li class="nav-item nav-user">
-          <div class="icon-area">
-            <font-awesome-icon icon="user"></font-awesome-icon>
-          </div>
-          <p>마이페이지</p>
-        </li>
+
       </ul>
     </nav>
   </div>
@@ -32,35 +19,85 @@
 <script>
 export default {
   name: 'Navigation',
+  props: {
+    name: {
+      type: String,
+      default: 'home',
+    },
+  },
+  data() {
+    return {
+      navItems: [
+        {
+          name: 'Home',
+          title: '홈',
+          path: '/',
+          icon: 'home',
+        },
+        {
+          name: 'Wish',
+          title: '찜 목록',
+          path: 'wish',
+          icon: 'heart',
+        },
+        {
+          name: 'Cart',
+          title: '장바구니',
+          path: 'cart',
+          icon: 'shopping-cart',
+        },
+
+        {
+          name: 'Mypage',
+          title: '마이페이지',
+          path: 'mypage',
+          icon: 'user',
+        },
+      ],
+    };
+  },
+  methods: {
+    pushRouter(path) {
+      this.$router.push(path);
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper{
+.nav-wrapper {
   position: fixed;
-  width:100%;
-  bottom:0;
-  left:0;
-  right:0;
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  right: 0;
 }
-.nav-list{
-  display:flex;
+
+.nav-list {
+  display: flex;
   align-items: stretch;
-  border-top:1px solid #e6e6e6;
-  padding:10px 0;
-  background:#fff;
-  .nav-item{
-    flex:1;
-    text-align:center;
-    color: #807d7d;
-    .icon-area{
-      width:100%;
-      margin-bottom:5px;
-      svg{
-        font-size:18px;
+  border-top: 1px solid #e6e6e6;
+  padding: 10px 0;
+  background: #fff;
+
+  .nav-item {
+    flex: 1;
+    text-align: center;
+    color: #666;
+    &.is-active{
+      color:$red_1;
+      font-weight:bold;
+    }
+    .icon-area {
+      width: 100%;
+      margin-bottom: 5px;
+
+      svg {
+        font-size: 18px;
       }
     }
-    font-size:13px;
+
+    font-size: 13px;
   }
 }
 </style>
