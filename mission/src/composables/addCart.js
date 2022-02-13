6ebe496bb174fdef414eaaf562d7d66694a3485d
yein <gpg7913@gmail.com>
2022-02-14 00:53:37 +0900

    Fix : addCart composable import store 오류 수정

--- mission/src/composables/addCart.js
+++ mission/src/composables/addCart.js
@@ -1,4 +1,4 @@
-import { useStore } from 'vuex';
+import store from '@/store/index';
 
 const addCart = (addItem) => {
   const addCartItemInfo = {
@@ -9,15 +9,16 @@ const addCart = (addItem) => {
     original_price: addItem.original_price,
     description: addItem.description,
   };
-  const store = useStore();
   const alreadyItemIndex = store.state.cartModule.cartList
     .findIndex((storeItem) => storeItem.product_no === addCartItemInfo.product_no);
   if (alreadyItemIndex === -1) {
-    store.dispatch('addCart', { ...addCartItemInfo, amount: 1 });
-    alert('상품이 장바구니에 추가되었습니다.');
+    store.dispatch('addCart', { ...addCartItemInfo, amount: 1 }).then(() => {
+      alert('상품이 장바구니에 추가되었습니다.');
+    });
   } else {
-    store.dispatch('increaseCart', alreadyItemIndex);
-    alert('상품이 이미 담겨있어 수량이 추가되었습니다.');
+    store.dispatch('increaseCart', alreadyItemIndex).then(() => {
+      alert('상품이 이미 담겨있어 수량이 추가되었습니다.');
+    });
   }
 };
 export default addCart;
